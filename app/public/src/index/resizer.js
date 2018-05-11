40503afa695d25559700c30d321e92d52742f109
Ph0enixKM <pkaras.it@gmail.com>
2018-05-11 19:27:16 +0200

    Added fundaments for resizer logic

--- app/public/src/index/resizer.js
+++ app/public/src/index/resizer.js
@@ -10,15 +10,20 @@ window.resizer = {
 
 resizer.update = (offset) => {
   // Top
-  resizer.top.style.top = resizer.body.offsetTop - resizer.offset - resizer.body.scrollTop + 107 + margins.top + offset + "px"
+  resizer.top.style.top = textField.offsetTop - resizer.body.scrollTop - offset - 5 + margins.top + "px"
+  resizer.top.style.left = textField.offsetLeft + "px"
+  // Left
+  resizer.left.style.top = textField.offsetTop - resizer.body.scrollTop - offset - margins.top + "px"
+  resizer.left.style.left = textField.offsetLeft + margins.left + "px"
   // Bottom
-  // resizer.bottom.style.top = textField.offsetTop + textField.offsetHeight - resizer.offset + 170 + "px"
+  resizer.bottom.style.top = textField.offsetTop + textField.offsetHeight - resizer.body.scrollTop - offset - margins.bottom + "px"
+  resizer.bottom.style.left = textField.offsetLeft + "px"
 }
-resizer.update(0)
+resizer.update(18)
 
-resizer.body.addEventListener('scroll', () => resizer.update(18))
+resizer.body.addEventListener('scroll', () => resizer.update(0))
 
 resizer.top.addEventListener('click', () => {
   margins.top = 0
-  resizer.update(18)
+  resizer.update(0)
 })
