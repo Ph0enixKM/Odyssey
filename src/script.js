47503eb36c61b4b69551c546fb4a8321129e5b8c
Ph0enixKM <pkaras.it@gmail.com>
2018-06-06 20:26:29 +0200

    Page Enchanted

--- src/script.js
+++ src/script.js
@@ -1,8 +1,5 @@
 function contAnim () {
   let cont = document.querySelector('#cont')
-  setTimeout(() => {
     cont.style.opacity = 1
     cont.style.transform = 'translate(0,0)'
-  }, 3000);
 }
-contAnim()
