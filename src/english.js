f21ef0e34dea45321571901d5fe58b3c9e06c2e2
Ph0enixKM <pkaras.it@gmail.com>
2018-07-16 10:13:52 +0200

    Improved website

--- src/english.js
+++ src/english.js
@@ -12,9 +12,9 @@ const english = () => {
     if (key != 0) ver += item + ' '
   })
   qs('#download').innerHTML = 'Version ' + ver
-  html('#title[uno]','Come to the dark side, the dark layout will not strain your eyes')
-  html('#title[dos]','Manipulate pages as if they were pieces of paper')
-  html('#title[tres]','Enjoy your own text editor, for which you do not have to pay anything')
+  html('#title[uno]','Dark Layout, Dusky design')
+  html('#title[dos]','Freedom to manipulate pages')
+  html('#title[tres]','Free usage')
   html('#title.havana','Contact in case of problems and bugs')
   html('.footer','Copyright © All rights reserved')
 }
